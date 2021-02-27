e7b14389c406f61ccd6ddbb422bba96e7d966051
Germain GUEDOU <prugermain@yahoo.fr>
2021-02-27 01:53:24 +0100

    Ajoute fichiers entête

---
+++ entete.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="entete.css" />
+        <title>Colocations à Paris et dans toute l'Île-de-France | MyColoc</title>
+    </head>
+
+    <body>
+
+          <header>
+                 <div id="logo">
+                     <a href="index.php"><img src="images/logo.png" alt="Logo du site" /></a>
+                 </div>
+                 
+                 <div id="titre_principal">
+                      <h1>Trouvez des colocations à<br/> Paris et dans toute l'Île-de-France</h1>
+                 </div> 
+                  
+                 <nav>
+                     <ul>
+                        <li><a href="index.php">Accueil</a></li>
+                        <li><a href="inscription.php">Inscription</a></li>
+                        <li><a href="connexion.php">Connexion</a></li>
+                        <li><a href="annonces.php" id="bouton_annonce">Publier votre annonce</a></li>
+                    </ul>
+                 </nav>
+          </header>
+
+    </body>
+</html>
